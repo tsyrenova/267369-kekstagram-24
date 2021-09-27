502e2fe007706081bb755a8f2f95203b1832bc9a
Olga Tsyrenova <olg_a88@mail.ru>
2021-09-27 17:11:13 +0300

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-export const getRandomIntNumber = (from, to) => {
+ export const getRandomIntNumber = (from, to) => {
+  if (from < 0 || to < 0) {
+    console.error('Минимальное и максимальное значение должны быть больше 0');
+    return;
+  }
+  if (from > to) {
+    console.error('Максимальное значение должно быть больше, чем минимальное');
+    return;
+  }
+
   const rand = from + Math.random() * (to + 1 - from);
-  return Math.abs(Math.floor(rand));
+  return Math.floor(rand);
 };
 
-export const checkMaxStringLength = (str, maxLength) => str.length < maxLength;
+export const checkMaxStringLength = (str, maxLength) => str.length <= maxLength;
